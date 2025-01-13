69dc74a374eae94931c6136aa06528c741701921
Yerya <153219286+Yerya@users.noreply.github.com>
2025-01-13 19:01:37 +0200

    feat: hrjs22-16 add logging and correlation ID tracing (#30)

--- apps/user/src/user.module.ts
+++ apps/user/src/user.module.ts
@@ -1,19 +1,42 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule, LoggerErrorInterceptor } from 'nestjs-pino';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User, UserSchema } from './user.schema';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, // Make environment variables available globally
-    }),
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                target: 'pino-pretty',
+                options: {
+                  colorize: true,
+                  translateTime: true,
+                },
+              }
+            : undefined,
+        customProps: (req, res) => {
+          return { correlationId: (req as any).correlationId };
+        },
+      },
+    }),
   ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerErrorInterceptor,
+    },
+  ],
 })
 export class UserModule {}
